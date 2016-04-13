d567eeb6df9852a09379caceaac2bd8c36713b73
DanJarvis <Danwakeem@yahoo.com>
2016-04-13 13:34:53 -0500

    Updated php files to match new statuses

--- waiterNav.php
+++ waiterNav.php
@@ -3,7 +3,7 @@
             <nav class="blog-nav">
                 <a class="blog-nav-item active" href="#">Table list</a>
                 <a class="blog-nav-item" href="#">Menu</a> 
-                <a class="blog-nav-item" href="#">Hello <?php $_SESSION['username']; ?></a>
+                <a class="blog-nav-item" href="#">Hello <?php echo $_SESSION['username']; ?></a>
             </nav>
         </div>
     </div>
