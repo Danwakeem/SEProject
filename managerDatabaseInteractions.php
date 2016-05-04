9058edadab5b554101c23cf1e87381c01f7799d1
Michael Hummer <mihaelhummer1@gmail.com>
2016-05-04 02:45:14 -0500

    added function to get daily orders to managerDatabaseInteractions.php

--- managerDatabaseInteractions.php
+++ managerDatabaseInteractions.php
@@ -68,5 +68,29 @@
 				return $result;
 			}
 		}
+		
+		function getWaiterName($id){
+			$con = dbConnect();
+			$sql = "SELECT username from user where id = ?";
+			$stmt = $con->prepare($sql);
+			$stmt->bind_param('s',$id);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$stmt->close();
+			return $result->fetch_assoc();
+		}
+		function getDailyOrders(){
+			$con=dbConnect();
+			$sql = "SELECT * FROM orders 
+					INNER JOIN orderItems ON orders.id=orderItems.orderId 
+					INNER JOIN menuItems ON orderItems.menuId=menuItems.id 
+					INNER JOIN menuItemCategory ON menuItems.id=menuItemCategory.menuItemId 
+					INNER JOIN category ON menuItemCategory.categoryId=category.id 
+					WHERE DATE(orders.date)=CURDATE()";
+			$stmt = $con->prepare($sql);
+			$stmt->execute();
+			$result = $stmt->get_result()
+			return $result;
+		}
 
 ?>
